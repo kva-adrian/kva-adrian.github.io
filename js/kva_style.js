1d9a37456b061b48119871d9ac2a685a661bd234
kva-adrian <adrian.fernandes@kvaliteta.in>
2019-01-14 18:50:46 +0530

    Initial commit

---
+++ js/kva_style.js
@@ -0,0 +1,24 @@
+$("nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
+
+$("nav ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 1000, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
