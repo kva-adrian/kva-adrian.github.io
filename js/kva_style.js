4f57679d4a79b031e06a15d8d40f3355b7de4fda
kva-adrian <adrian.fernandes@kvaliteta.in>
2019-01-16 19:54:27 +0530

    updates to navigation

--- js/kva_style.js
+++ js/kva_style.js
@@ -1,24 +1,11 @@
-$("nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
+/* affix the 2nd navbar after scroll past first navbar */
+$('#nav2').affix({
+      offset: {
+        top: 35
+      }
+});	
 
-$("nav ul li a[href^='#']").on('click', function(e) {
 
-   // prevent default anchor click behavior
-   e.preventDefault();
 
-   // store hash
-   var hash = this.hash;
 
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 1000, function(){
 
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
