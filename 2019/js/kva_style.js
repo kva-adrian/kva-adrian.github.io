d9106f91da1496f6719d88c116cd70480a35c48f
kva-adrian <adrian.fernandes@kvaliteta.in>
2019-04-16 13:21:43 +0530

    Update kva_style.js

--- 2019/js/kva_style.js
+++ 2019/js/kva_style.js
@@ -1,53 +1,32 @@
+$(document).ready(function(){
+
+    //your code goes here
 $(function() {
     $('header').height($('nav').height());
     
     $('nav').affix({
         offset: { top: $('nav').offset().top }
     });
-
-$("#sdaasmenu a").click(function() {
-	$("#nav-lower").load("pillmenu.html #pillmenu-sdaas");
-	$(".dropdown ul").removeClass("hidden-lg hidden-md hidden-sm");
-	$("#sdaasmenu ul").addClass("hidden-lg hidden-md hidden-sm");
-  });
-$("#entitmenu a").click(function() {
-	$("#nav-lower").load("pillmenu.html #pillmenu-entit");
-	$(".dropdown ul").removeClass("hidden-lg hidden-md hidden-sm");
-	$("#entitmenu ul").addClass("hidden-lg hidden-md hidden-sm");
-});
-
-$("#spitmenu a").click(function() {
-    $("#nav-lower").load("pillmenu.html #pillmenu-spit");
-	$(".dropdown ul").removeClass("hidden-lg hidden-md hidden-sm");
-	$("#spitmenu ul").addClass("hidden-lg hidden-md hidden-sm");
-  }); 
-$("#partneringmenu a").click(function() {
-    $("#nav-lower").load("pillmenu.html #pillmenu-partnering");
-	$(".dropdown ul").removeClass("hidden-lg hidden-md hidden-sm");
-	$("#partneringmenu ul").addClass("hidden-lg hidden-md hidden-sm");
-  }); 
-$("#aboutmenu a").click(function() {
-    $("#nav-lower").load("pillmenu.html #pillmenu-about");
-	$(".dropdown ul").removeClass("hidden-lg hidden-md hidden-sm");
-	$("#aboutmenu ul").addClass("hidden-lg hidden-md hidden-sm");
-  });  
-$("#homepage").click(function() {
-	$("#nav-lower").empty();
-	$(".dropdown ul").removeClass("hidden-lg hidden-md hidden-sm");
-	$("nav li").removeClass("active");
-  });  
-
+});//end main function
 
 $("#nav2 li").on("click", function() {
   $("#nav2 li").removeClass("active");
   $(this).addClass("active");
 });
-/*  $("#nav-lower .nav li").on("click", function() {
-  $("#nav2 li.active").removeClass("active");
-  $("#nav-lower .nav li").addClass("active");
-}); */
+
+$(function() {
+    $('a[data-toggle="tab"]').on('click', function(e) {
+        window.localStorage.setItem('activeTab', $(e.target).attr('href'));
+    });
+    var activeTab = window.localStorage.getItem('activeTab');
+    if (activeTab) {
+		$('#addresses a[href="' + activeTab + '"]').tab('show');
+        window.localStorage.removeItem("activeTab");
+    }
 });
 
+
+});
 
  
 /* $("nav li ul li").on("click", function() {
