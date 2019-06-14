880df4d485c7b8aa5a8a16817b1bcca168040bed
kva-adrian <adrian.fernandes@kvaliteta.in>
2019-06-14 12:28:25 +0530

    v3 idcheck

---
+++ idcheck/precache-manifest.88813bd12d36c15738f4f37a9a1577f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4857f1d6eeaff516e06a09d82369103e",
+    "url": "https://kva-adrian.github.io/idcheck/index.html"
+  },
+  {
+    "revision": "4d532d52a6b4d4898023",
+    "url": "https://kva-adrian.github.io/idcheck/static/css/main.331a09a8.chunk.css"
+  },
+  {
+    "revision": "0c194b958d086b805203",
+    "url": "https://kva-adrian.github.io/idcheck/static/js/2.f4da53a6.chunk.js"
+  },
+  {
+    "revision": "4d532d52a6b4d4898023",
+    "url": "https://kva-adrian.github.io/idcheck/static/js/main.f70fec59.chunk.js"
+  },
+  {
+    "revision": "2c82900ddfc716e8f33b",
+    "url": "https://kva-adrian.github.io/idcheck/static/js/runtime~main.617c9488.js"
+  },
+  {
+    "revision": "5abb9c9f64f6ec8d298dbe0ce17eb025",
+    "url": "https://kva-adrian.github.io/idcheck/static/media/CID-banner.5abb9c9f.png"
+  },
+  {
+    "revision": "0c7248855fa32cf64ed57349a6fb6df4",
+    "url": "https://kva-adrian.github.io/idcheck/static/media/card_back@3x.0c724885.png"
+  },
+  {
+    "revision": "d268a33dd7020770c9be64f14571759a",
+    "url": "https://kva-adrian.github.io/idcheck/static/media/illustration1@3x.d268a33d.png"
+  },
+  {
+    "revision": "406cb2b985fcbd5dca1111b268798326",
+    "url": "https://kva-adrian.github.io/idcheck/static/media/illustration2@3x.406cb2b9.png"
+  },
+  {
+    "revision": "5e4db5272f8a48d5505821f0fb762833",
+    "url": "https://kva-adrian.github.io/idcheck/static/media/image_correct.5e4db527.jpg"
+  },
+  {
+    "revision": "0b1f3e62b46ab75ae3ea73edf8f65063",
+    "url": "https://kva-adrian.github.io/idcheck/static/media/scan_process.0b1f3e62.gif"
+  }
+]);
